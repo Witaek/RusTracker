cdbe56bae3cd69f2e13b9d22d8a5d1fc5898f352
Witaek <amedee.romain@gmail.com>
2022-02-11 01:21:02 +0100

    Fonction qui, à partir du message binaire de 56 bits, donne le numéro du vol

--- ADSB/src/main.rs
+++ ADSB/src/main.rs
@@ -1,3 +1,41 @@
+use std::str;
+
+const ALPHABET: &str= "#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######";
+
+
 fn main() {
-    println!("Hello, world!");
+    let bin: u64 = 0b00100000001011001100001101110001110000110010110011100000;
+
+    let number = callsign(&bin);
+    println!("{}",number);
+
+}
+
+fn callsign(&bin: &u64) -> String {
+    let bin_str: String = format!("{bin:056b}");
+
+    let mut arr: [&str;10] = ["";10];
+    arr[0] = &bin_str[..5];
+    arr[1] = &bin_str[5..8];
+
+    let mut index = 2;
+    for i in [8, 14, 20, 26, 32, 38, 44, 50] {
+        arr[index] = &bin_str[i..i+6];
+        
+        index += 1;
+    }
+
+    get_flight_number(arr)
+}
+
+fn get_flight_number(arr: [&str;10]) -> String {
+
+    let mut flight_number: String = String::from("");
+    for elem in &arr[2..] {v
+        let index: usize = usize::from_str_radix(elem, 2).expect("Not binary");
+        //let letter: u8 = bytes_alphabet[index];
+        let letter = &ALPHABET[index..index+1];
+        flight_number.push_str(letter)
+    };
+    flight_number
 }
