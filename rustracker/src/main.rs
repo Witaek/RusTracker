9ac1952d9d7a29695e5736735c278def77a084ab
Witaek <amedee.romain@gmail.com>
2022-03-07 18:20:24 +0100

    reception test

--- rustracker/src/main.rs
+++ rustracker/src/main.rs
@@ -9,4 +9,24 @@ mod test;
 
 //main
 fn main() {
+
+    let (mut ctl, mut reader) = rtlsdr_mt::open(0).unwrap();
+
+    ctl.enable_agc().unwrap();
+    ctl.set_ppm(-2).unwrap();
+    ctl.set_center_freq(774_781_250).unwrap();
+
+    std::thread::spawn(move || {
+        loop {
+            let next = ctl.center_freq() + 1000;
+            ctl.set_center_freq(next).unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+
+    reader.read_async(4, 32768, |bytes| {
+        println!("i[0] = {}", bytes[0]);
+        println!("q[0] = {}", bytes[1]);
+    }).unwrap();
 }
